bdbe6e428bb027da92d813ce5ed98be3e61ac6b3
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-08-31 22:13:01 -0700

    finished rich Text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Box, Grid, Text, Image } from "@chakra-ui/react"
 import Hero from "../components/Hero"
 import { ShopContext } from "../context/shopContext";
 import ImageWithText from "../components/ImageWithText";
+import RichText from '../components/RichText';
 
 const Home = () => {
 const { fetchAllProducts, products } = useContext(ShopContext);
@@ -17,22 +18,24 @@ if(!products) return <div>Loading....</div>
 return (
     <Box>
         <Hero />
+        <RichText heading="The relaxation you’ve been waiting for." text="Our Bath bombs guarantee a fun, relaxing, and colorful night."/>
+
         <Grid templateColumns="repeat(3,1fr)" >
             { products.map(product => (
                 <Link to={`/products/${product.handle}`} key={product.id}>
-                    <Box _hover={{ opacity: "80%" }} textAlign="center">
+                    <Box _hover={{ opacity: "80%" }} textAlign="center" position="relative">
                         <Image src={product.images[0].src} />
-                        <Text>
+                        <Text position="absolute" bottom="15%" w="100%" fontWeight="bold">
                             {product.title}
                         </Text>
-                        <Text>
+                        <Text position="absolute" bottom="5%" w="100%" color="gray.500">
                             ${product.variants[0].price}
                         </Text>
                     </Box>
                 </Link>
             ))}
         </Grid>
-
+    <RichText heading="Treat yourself!" />
     <ImageWithText 
         button
         image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758"
